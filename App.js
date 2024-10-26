1c83a883c382fc134ae1fbf8a15e35f4ca60799e
sreevani <sreevanigaddam1010@gmail.com>
2024-10-26 13:03:53 +0530

    add how jsx convert to js

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-// const heading = React.createElement("h1",{id:"heading"},"Hello world from React");
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-    const jsxHeading = <h1 className="heading">Namaste reactbbb</h1>
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+//React.createElement => Object => Html element (render)
 
-    root.render(jsxHeading);
+const heading = React.createElement("h1", {}, "Reactttt");
+
+//JSX (transpiled before it reaches the JS) - PARCEL give responsibility to transpiler babel - BABEL
+
+//JSX => Babel transapiler convert it to => React.createElement => ReactElement.JS Object => HtmlElement (render)
+
+//to write jsx in multiple lines we can wrap elements into ()
+const jsxHeading = (
+<h1 className="heading">
+    Namaste reactbb
+</h1>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
