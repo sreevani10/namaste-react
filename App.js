be04ec020b4bc725ef2d719f363900a90794508b
sreevani <sreevanigaddam1010@gmail.com>
2024-10-26 12:50:40 +0530

    add jszx element to dom

--- App.js
+++ App.js
@@ -2,17 +2,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div",{},[
-    React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"This is namaste react"),
-        React.createElement("h2",{},"Iam h2 tag")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"Iam h2 tag")
-    ])
-])
 
+    const jsxHeading = <h1 className="heading">Namaste reactbbb</h1>
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(parent);
+    root.render(jsxHeading);
