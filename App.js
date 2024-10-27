e7259a4f3b763e9209b37d883769412b0ff34bf1
sreevani <sreevanigaddam1010@gmail.com>
2024-10-27 08:44:05 +0530

    use of {} in react

--- App.js
+++ App.js
@@ -1,41 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement => Object => Html element (render)
-
-const heading = React.createElement("h1", {}, "Reactttt");
-
-//JSX (transpiled before it reaches the JS) - PARCEL give responsibility to transpiler babel - BABEL
-
-//JSX => Babel transapiler convert it to => React.createElement => ReactElement.JS Object => HtmlElement (render)
-
-//to write jsx in multiple lines we can wrap elements into ()
-
-//React Element
-const jsxHading = (
-<h1 className="heading">
-    Namaste reactbb
-</h1>
-);
 
 
-///Component Composition -- to put one component into another
-const Title =()=> (
+const title =(
     <h1 className="heading">
         Namaste reactbb
     </h1>
     );
 
-//React Component
-// Class based com and functional based com
-//React Functional Component
+    const data = 1000;
 const HeadingComponent = ()=>(
      <div id="container">
-        <Title/> 
-
+        {data}
+        {title}
      <h1>React Functional Component</h1>
      </div>
 )
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent/>);
