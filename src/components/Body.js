796c8d806132ee49ed31f34160c0b0e5cd9f0a54
sreevani <sreevanigaddam1010@gmail.com>
2024-11-10 19:29:23 +0530

    add use state hook to component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,53 +1,16 @@
 import RestaurantCard from "./RestaurantCard";
 import restList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-  let listOfRestaurants = [
-    {
-      info: {
-        id: "588613",
-        name: "KFC",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/5/ab13ff98-4cb5-4062-bef6-1738d5d5ed45_588619.JPG",
-        locality: "Brigade Road",
-        areaName: "Central Bangalore",
-        costForTwo: "₹400 for two",
-        cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
-        avgRating: 3.6,
-        parentId: "547",
-        avgRatingString: "4.4",
-        totalRatingsString: "2.0K+",
-        sla: {
-          deliveryTime: 50,
-        },
-      },
-    },
-    {
-        info: {
-          id: "588619",
-          name: "Dominos",
-          cloudinaryImageId:
-            "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/5/ab13ff98-4cb5-4062-bef6-1738d5d5ed45_588619.JPG",
-          locality: "Brigade Road",
-          areaName: "Central Bangalore",
-          costForTwo: "₹400 for two",
-          cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
-          avgRating: 4.4,
-          parentId: "547",
-          avgRatingString: "4.4",
-          totalRatingsString: "2.0K+",
-          sla: {
-            deliveryTime: 50,
-          },
-        },
-    }
-  ];
+  let [listOfRestaurants,setListOfRestaurants] = useState(restList);
   return (
     <div className="body">
       <div className="filter">
         <button className="filter-btn" onClick={() => {
-            listOfRestaurants = listOfRestaurants.filter((res)=> res.info.avgRating>4);
-            console.log(listOfRestaurants);
+            let filteredList = listOfRestaurants.filter((res)=> res.info.avgRating>4);
+            setListOfRestaurants(filteredList);
+            console.log(filteredList);
         }}>
           Top Rated Restaurants
         </button>
