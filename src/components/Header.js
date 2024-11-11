dcf2bc68a027b81cd5c98d09d9509fb4c5d2110f
sreevani <sreevanigaddam1010@gmail.com>
2024-11-11 19:05:00 +0530

    add login button and update state using useState hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+    const[btnName,setBtnName]=useState("Login")
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,7 @@ const Header = () => {
           <li>About us</li>
           <li>Contact us</li>
           <li>Cart</li>
+          <button onClick={()=>{btnName ==="Login" ? setBtnName("Logout") : setBtnName("Login")}}>{btnName}</button>
         </ul>
       </div>
     </div>
