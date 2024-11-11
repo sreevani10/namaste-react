44b3843bcc38a394fcbfb91eb371252e59419a82
sreevani <sreevanigaddam1010@gmail.com>
2024-11-11 17:22:27 +0530

    implement useEffect hook for api data fetching

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,37 @@
 import RestaurantCard from "./RestaurantCard";
 import restList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  let [listOfRestaurants,setListOfRestaurants] = useState(restList);
+  let [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.926115835828364&lng=77.63606936931151&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setListOfRestaurants(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {
-            let filteredList = listOfRestaurants.filter((res)=> res.info.avgRating>4);
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
             setListOfRestaurants(filteredList);
-            console.log(filteredList);
-        }}>
+          }}
+        >
           Top Rated Restaurants
         </button>
       </div>
