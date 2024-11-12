8bb7d0189a2c9a90cd86ae59d95aca4ec373a6b8
sreevani <sreevanigaddam1010@gmail.com>
2024-11-12 14:45:55 +0530

    implement routing using createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import { createBrowserRouter , RouterProvider , Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 const AppLayout = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet/>
     </div>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    children:[
+      {
+        path:"/",
+        element:<Body/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      }
+    ],
+    errorElement:<Error/>
+  },
+  
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter}/>)
