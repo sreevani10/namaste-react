069e85f2c9a7f18b910e7ffa4e0e3828f8e7d23f
sreevani <sreevanigaddam1010@gmail.com>
2024-12-05 14:13:06 +0530

    modify url as constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
-
-
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL = "";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9265132&lng=77.63615519999999&restaurantId=";
