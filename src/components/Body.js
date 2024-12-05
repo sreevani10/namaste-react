1bcc6715273d752dc3f136c43646bed6d80357f8
sreevani <sreevanigaddam1010@gmail.com>
2024-12-05 14:58:05 +0530

    create useOnlineStatus custom hook and add to header component

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,13 @@ import restList from "../utils/mockData";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   let [listOfRestaurants, setListOfRestaurants] = useState([]);
   let [filteredRestaurant, setFilteredRestaurant] = useState([]);
   let [searchText, setSearchText] = useState("");
+
 
   useEffect(() => {
     fetchData();
@@ -27,6 +29,13 @@ const Body = () => {
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
+
+  const onlineStatus = useOnlineStatus();
+
+  if(onlineStatus === false)
+    return(
+       <h1>Looks like you're offline !! Please check your internet connection</h1>
+  )
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
-    const[btnName,setBtnName]=useState("Login")
+    const[btnName,setBtnName]=useState("Login");
+    const onlineStatus = useOnlineStatus();
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +14,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status : {onlineStatus ? "✅": "🔴"}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
