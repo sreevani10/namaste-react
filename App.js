0d50d91db51c1dab2567aa3ea51ff2d1197f8328
sreevanigaddam <sreevani.gaddam@zyter.com>
2025-10-14 12:08:49 +0530

    "episode-2"

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-/*
-<div id="parent">
-<div id="child">
-<h1>I am h1 tag</h1>
-<h2>I am h2 tag</h2>
-</div>
-<div id="child2">
-<h1>I am h1 tag</h1>
-<h2>I am h2 tag</h2>
-</div>
-</div>
-
-
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "react"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -24,12 +12,5 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-//jsx
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello world from React!"
-// );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Namaste React
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm
+- Caching - Faster Build
