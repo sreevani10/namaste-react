16f7aa9d276c86cd9aa7ad9de32025a16dc773d7
sreevanigaddam <sreevani.gaddam@zyter.com>
2025-10-17 11:21:09 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "react"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
+//React.createElement => React Element => Object =>HTML Element(rendered in the DOM)
 
+const heading = React.createElement("h1", { id: "title" }, "Namaste React");
+
+console.log(heading);
+
+// JSX => html like syntax
+//JSX (transpiled before it reaches to js enginer or browser) => PARCEL => BABEL
+//JSX => Babel transpiles it to
+// \React.createElemnt => React Element => Object => HTML Element (render)
+
+const Title = () => <h1 className="head">Namaste React using JSX</h1>;
+
+//React Components
+//Functional and Class based Components
+
+// Component Composition => Composing 2 components into one another
+const HeaderComponet = () => (
+  <div>
+    <Title />
+    <h1>React Funcional Component</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeaderComponet />);
